8002ca0a208ae4649d4e0f91d4713d5235ca9aca
Kyle Oberholzer <kyleoberholzer@gmail.com>
2024-11-06 14:41:54 +0100

    changed to react hooks git status clear

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const Card = ({name, email, id}) => {
 return(
     <div className="bg-light-blue dib br2 pa2 ma1 tc grow bw1 shadow-3"> 
-        <img alt='photo' src={`https://robohash.org/${id}`}/>
+        <img alt='robot' src={`https://robohash.org/${id}`}/>
         <div>
             <h2>{name}</h2>
             <p>{email}</p>
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,49 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from "../Components/CardList";
-import { robots } from '../Components/robots';
+
 import SearchBox from '../Components/SearchBox';
 import './App.css';
 import Scroll from '../Components/Scroll';
 
-class App extends Component {
-    
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchField: ""
-        }
-    }
+function App () {
 
-    componentDidMount(){
+    const [robots, setRobots] = useState([]); //creating state for robots
+    const [searchField, setSearchField] = useState(''); //creating state for searchfield
+    const [count, setCount] = useState (0); // creates a state fo the button
+
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {this.setState({robots: users})});
+        .then(response => response.json())
+        .then(users => {setRobots(users)});
+        console.log(count);
+    }, [count] )
+
+    const onSearchChange = (event) => {         // function to set the searchfield
+        setSearchField(event.target.value )
     }
 
-    onSearchChange = (event) => {
-        this.setState ({ searchField: event.target.value })
+    const filteredRobots = robots.filter(robot => { // function to set the filtered Robot array
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    if (!robots.length){ //checks if robots has a length in case it's still loading behind
+        return <h1>Loading...</h1>
+    } else {
+        return(
+            <div className='tc'>
+                <h1 className = 'sega-logo'>Robofriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+                <button onClick={() =>setCount(count+1)}>Click me</button>
+                <Scroll>
+                <CardList robots = { filteredRobots } />
+                </Scroll>
+            </div>
+        )
     }
     
-    render () {
-        const { robots, searchField } = this.state;
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        })
-        if (!robots.length){ //checks if robots has a length
-            return <h1>Loading...</h1>
-        } else {
-            return(
-                <div className='tc'>
-                    <h1 className = 'sega-logo'>Robofriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                    <CardList robots = { filteredRobots } />
-                    </Scroll>
-                </div>
-            )
-        }
-    }
 }
 
 export default App; 
